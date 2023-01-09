a2006d71485abef418692972a2394a5f38608a70
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2023-01-09 14:53:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// Проміс
-// const fetchUserFromServer = (username) => {
-//   return new Promise((resolve, reject) => {
-//     console.log(`Fetching data for ${username}`);
-
-//     setTimeout(() => {
-//       // Change value of isSuccess variable to simulate request status
-//       const isSuccess = false;
-
-//       if (isSuccess) {
-//         resolve("success value");
-//       } else {
-//         reject("someting");
-//       }
-//     }, 2000);
-//   });
-// };
-
-// fetchUserFromServer("Mango")
-//   .then((user) => console.log(user))
-//   .catch((error) => console.log(error));
-
 const fetchImage = async (searchedImages) => {
   const response = await fetch(
     `https://pixabay.com/api/?key=32715422-e0410e3c137bf18af69487d41&q=${searchedImages}&image_type=photo&orientation=horizontal&safesearch=true`
@@ -29,19 +7,30 @@ const fetchImage = async (searchedImages) => {
 };
 // fetchImage().then((images) => console.log(images));
 
-const input = document.querySelector('[name="searchQuery"]');
-input.addEventListener("input", onInput);
+// const input = document.querySelector('[name="searchQuery"]');
+// const submitBtn = document.querySelector("button");
+// input.addEventListener("input", onInput);
+// submitBtn.addEventListener("submit", () => {
+//   console.log("ok");
+// });
 
-let mage = "";
-function onInput() {
-  mage = input.value;
-  console.log(mage);
+// function onInput() {
+//   const searchedImages = input.value;
+// }
+// function onSubmit() {
+//   console.log("ok");
+// }
+// function createImageMarkup() {}
+
+const form = document.querySelector("#search-form");
+
+form.addEventListener("submit", onSubmit);
+function onSubmit(evt) {
+  evt.preventDefault();
 }
 
-// input.addEventListener("submit", onSubmit);
-
-// const searchedImage = "";
-
-// function onSubmit() {}
-
-// function createImageMarkup() {}
+form.addEventListener("input", onInput);
+function onInput(evt) {
+  const { searchQuery } = evt.currentTarget.elements;
+  console.log(searchQuery.value);
+}
