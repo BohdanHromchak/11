f661e35206f44e77b4385f77ae9412278d9f710c
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2023-01-09 12:14:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,11 @@
 //   .then((user) => console.log(user))
 //   .catch((error) => console.log(error));
 
-const fetchImage = async () => {
-    const response = 
+const fetchImage = async (searchedImages) => {
+  const response = await fetch(
+    `https://pixabay.com/api/?key=32715422-e0410e3c137bf18af69487d41&q=${searchedImages}&image_type=photo&orientation=horizontal&safesearch=true`
+  );
+  const images = await response.json();
+  return images;
 };
+fetchImage().then((images) => console.log(images));
