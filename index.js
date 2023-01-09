327c734c03551eb26576ad824debec806fe2a358
BohdanHromchak <110772070+BohdanHromchak@users.noreply.github.com>
2023-01-09 11:38:47 +0200

    updated

---
+++ index.js
@@ -0,0 +1,25 @@
+// Проміс
+// const fetchUserFromServer = (username) => {
+//   return new Promise((resolve, reject) => {
+//     console.log(`Fetching data for ${username}`);
+
+//     setTimeout(() => {
+//       // Change value of isSuccess variable to simulate request status
+//       const isSuccess = false;
+
+//       if (isSuccess) {
+//         resolve("success value");
+//       } else {
+//         reject("someting");
+//       }
+//     }, 2000);
+//   });
+// };
+
+// fetchUserFromServer("Mango")
+//   .then((user) => console.log(user))
+//   .catch((error) => console.log(error));
+
+const fetchImage = async () => {
+    const response = 
+};
